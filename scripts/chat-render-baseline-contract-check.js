a0c4fea08231df889758591bbd1dcb979231f875
Shiyong Tan <tan_shiyong@126.com>
2026-07-21 14:31:04 -0400

    Normalize render contract line endings

--- scripts/chat-render-baseline-contract-check.js
+++ scripts/chat-render-baseline-contract-check.js
@@ -4,7 +4,7 @@ const path = require('path');
 const crypto = require('crypto');
 
 function read(relativePath) {
-  return fs.readFileSync(path.join(process.cwd(), relativePath), 'utf8');
+  return fs.readFileSync(path.join(process.cwd(), relativePath), 'utf8').replace(/\r\n/g, '\n');
 }
 
 function assert(condition, label) {
